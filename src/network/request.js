ccce2e195f78b20604609e7ea0b8a44ffd19d9f8
zhangyunan880 <1345718600@qq.com>
2022-08-18 21:34:17 +0800

    更新提交

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
     // 1.创建axios的实例?
     const instance = axios.create({
-        baseURL: 'http://152.136.185.210:7878/api/hy66',
+        baseURL: '自己去买',
         timeout: 5000
     })
 
